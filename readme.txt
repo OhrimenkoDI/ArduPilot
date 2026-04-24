c2cb77852deeb09f611123b98ee7fa2e9dce89d1
odi <ohrimenkodi@gmail.com>
2026-04-24 15:53:58 +0300

    Удалил лишнее

--- readme.txt
+++ readme.txt
@@ -8,3 +8,13 @@ python -m venv myenv
 pip install mavproxy
 python -m pip install prompt_toolkit
 python -m pip install wxPython
+
+///////////////////////////////////////
+На Orange:
+Включение UART3 (Orange Pi OS / Armbian)
+
+sudo nano /boot/orangepiEnv.txt
+Добавь строку:
+overlays=uart3-m1
+Если уже есть overlays= — добавь через пробел:
+overlays=uart3-m1 spi0-m1
--- start_mavproxy.bat
+++ start_mavproxy.bat
@@ -1,38 +1,5 @@
-@echo off
-setlocal
-
-set "ROOT=%~dp0"
-set "PYTHON=%ROOT%venv\Scripts\python.exe"
-set "MAVPROXY=%ROOT%venv\Scripts\mavproxy.py"
-
-if not exist "%PYTHON%" (
-    echo [ERROR] Python not found: "%PYTHON%"
-    pause
-    exit /b 1
-)
-
-if not exist "%MAVPROXY%" (
-    echo [ERROR] MAVProxy script not found: "%MAVPROXY%"
-    pause
-    exit /b 1
-)
-
-pushd "%ROOT%"
-
-echo Starting MAVProxy...
-echo Python: "%PYTHON%"
-echo Script: "%MAVPROXY%"
-echo.
-
-"%PYTHON%" "%MAVPROXY%" ^
-    --master=udp:0.0.0.0:14550 ^
-    --out=udp:127.0.0.1:14551 ^
+ .\venv\Scripts\python.exe -m MAVProxy.mavproxy 
+    --master=udpin:10.0.20.15:14550 
+    --out=udpout:10.0.20.177:14551 
+    --out=udpbcast:127.0.0.1:14552 
     --out=udpin:0.0.0.0:14553
-
-set "EXITCODE=%ERRORLEVEL%"
-popd
-
-echo.
-echo MAVProxy exited with code %EXITCODE%.
-pause
-exit /b %EXITCODE%
